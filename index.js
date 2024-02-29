774a1e55126fb94d83c6fb42ad839bcaf3522fc6
Levansarajishvili <levan.sarajishvili123@gmail.com>
2024-02-29 15:04:45 +0400

    just commerce

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
+
+
+
+
